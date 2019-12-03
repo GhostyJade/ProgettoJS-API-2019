13f27b7464c755bf6ed6d1e9c3aaf4ec06c969f9
GhostyJade <giandreas@outlook.it>
2019-12-03 14:27:50 +0100

    Added test javascript code

--- public/app.js
+++ public/app.js
@@ -1,4 +1,20 @@
 var retrievedQuestions = []
 
-fetch("https://opentdb.com/api.php?amount=10").then(response => response.json()).then(e => console.log(e))
+const image = document.getElementById("image")
+const text = document.getElementById("text")
+
+async function getQuestions() {
+    fetch("https://opentdb.com/api.php?amount=10")
+        .then(response => response.json())
+        .then(e => retrievedQuestions.push(e))
+}
+
+async function dispatchQuestion (question) {
+    const txt = question.question
+    const img
+    text.innerText = txt
+}
+
+await getQuestions()
+document.getElementById("game").onclick=await dispatchQuestion(retrievedQuestions[0])
 
