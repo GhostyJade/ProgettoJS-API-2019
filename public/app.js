694f85cdf514a3b0c93d5bdb874ecf3ef30a553e
GhostyJade <giandreas@outlook.it>
2019-12-03 14:51:46 +0100

    Made fetch partial working

--- public/app.js
+++ public/app.js
@@ -3,18 +3,20 @@ var retrievedQuestions = []
 const image = document.getElementById("image")
 const text = document.getElementById("text")
 
-async function getQuestions() {
+const getQuestions = () => {
     fetch("https://opentdb.com/api.php?amount=10")
         .then(response => response.json())
-        .then(e => retrievedQuestions.push(e))
+        .then(content => content.results.forEach(e=>retrievedQuestions.push(e)))
+        //.then(document.getElementById("game").onclick=dispatchQuestion(retrievedQuestions[0]))
 }
 
 async function dispatchQuestion (question) {
+    //console.log(question)
     const txt = question.question
-    const img
+    // const img
     text.innerText = txt
 }
 
-await getQuestions()
-document.getElementById("game").onclick=await dispatchQuestion(retrievedQuestions[0])
+
+getQuestions()
 
