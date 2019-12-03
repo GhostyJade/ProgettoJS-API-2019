4566584a31b6d965eff567db6c07dee3e54c84ba
GhostyJade <giandreas@outlook.it>
2019-12-03 17:40:35 +0100

    Broken everything.

--- public/app.js
+++ public/app.js
@@ -1,12 +1,22 @@
-var retrievedQuestions = []
-
 const image = document.getElementById("image")
 const text = document.getElementById("text")
 
+let questionDefinition = {
+    category: "", question: "" 
+}
+
 const getQuestions = () => {
+    var data = []
     fetch("https://opentdb.com/api.php?amount=10")
         .then(response => response.json())
-        .then(content => content.results.forEach(e=>retrievedQuestions.push(e)))
+        .then(content => {
+            var d = content.results
+            console.log(d.length)
+            for(i = 0; i < d.length; i++){
+                data.push(d[i])
+            }
+        })
+        .then(console.log(data.length))
         //.then(document.getElementById("game").onclick=dispatchQuestion(retrievedQuestions[0]))
 }
 
@@ -19,4 +29,3 @@ async function dispatchQuestion (question) {
 
 
 getQuestions()
-
